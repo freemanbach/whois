c885f744fab4e323556d2d0583dd3323bc1a4462
freemanbach <flo@radford.edu>
2023-12-12 09:49:41 -0500

    updated py script with better bad data capturing.

--- src/testWHOISServers.py
+++ src/testWHOISServers.py
@@ -1,38 +1,42 @@
 #!/usr/local/bin/env python
 
 """
-
-
+author  : freeman
+desc    : to check this list of world-wide whois servers
+        : of connectivity.
+date    : 20231211
 """
 
 import sys
 import requests as req
 
 def main():
-    cnt = 0
+    cnt, bad_servers = 0, []
     try:
         sf = sys.argv[1]
     except IndexError as err:
         print("no file Provided !")
+        sys.exit()
     with open(sf, mode="r") as f:
         for i in f:
             tmp = i.split(',')
             try:
                 ans = req.get("http://" +str(tmp[1]).rstrip("\n"))
             except req.exceptions.Timeout as to_err:
-                print(str(tmp[1]))
+                bad_servers.append(str(tmp[1]))
             except req.exceptions.TooManyRedirects as red_err:
-                print(str(tmp[1]))
+                bad_servers.append(str(tmp[1]))
             except req.exceptions.RequestException as e:
-                print(str(tmp[1]))
+                bad_servers.append(str(tmp[1]))
+                print(bad_servers)
                 sys.exit()
 
             if ans.ok == True:
-                print(str(cnt))
+                print(str(cnt) + "  " + str(tmp[1]).rstrip("\n") + "  ok.")
                 cnt+=1
                 continue
             else:
-                print(tmp[1])
+                bad_servers.append(str(tmp[1]))
 
 if __name__ == "__main__":
     main()
