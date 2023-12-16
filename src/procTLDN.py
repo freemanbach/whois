63ee3fb01f14d05b1021951c82b9160c9a3e6664
freemanbach <flo@radford.edu>
2023-12-16 01:25:04 -0500

    python script to cconvert data.csv to data form

---
+++ src/procTLDN.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/env python
+
+import sys
+
+def main():
+    cnt, fqdn = 1, []
+    fi = sys.argv[1]
+    with open(fi, mode="r") as f:
+        for i in f:
+            tmp = i.split(',')
+            sql = '(' + str(cnt) + ',' + "\"" + tmp[0].strip().lower() + "\"" + ',' + "\"" + tmp[1].strip().lower() + "\"" + "),"
+            fqdn.append(sql)
+            cnt+=1
+
+    for i in fqdn:
+        print(i)
+
+if __name__ == "__main__":
+    main()
